8198d11cf92855b137d7864c5cfb43e76a543edd
tkdans7589 <32192086+tkdans7589@users.noreply.github.com>
2021-10-06 14:10:16 +0900

    Create 2. k번째 수.py

---
+++ 섹션2/2. k번째 수.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  6 14:03:53 2021
+
+@author: tkdan
+"""
+
+T = int(input())
+for t in range(T):
+    n, s, e, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    a = a[s-1:e]
+    a.sort()
+    print("#%d %d" %(t+1, a[k-1]))
